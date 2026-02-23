b149659c60df5bf229795ab27706cd2282f7e997
shaye <sm0548540902@gmail.com>
2026-02-23 10:23:13 +0200

    add border

--- src/companentas/gridLayout.jsx
+++ src/companentas/gridLayout.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function gridLayout() {
-  return (
-    <div>
-      
-    </div>
-  )
+function GridLayout() {
+    const arr = [1,2,3,4,5,6,7,8,9,10]
+    const [click,setClick] = useState(0)
+    return (
+        <div id='fCom'>
+            <div id='header'>
+                <h1 id='fH'>Find The</h1>
+                <h1 id='lh'>Secret Color!</h1>
+                <div id='red'></div>
+            </div>
+            <div id='board'>
+               {arr.map(()=>{
+                return(
+                  arr.map(()=>{
+                    return(
+                        <div id='square'></div>
+                    )
+                  })
+                )
+               })}
+            </div>
+            <div>
+                <h1 id='click'>Clicks: {click}</h1>
+            </div>
+            <div>
+                <h2 id='keep'>Keep Searching!</h2>
+            </div>
+        </div>
+    )
 }
 
-export default gridLayout
+export default GridLayout
